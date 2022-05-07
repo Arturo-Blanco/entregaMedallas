9c8d7c8d40f1086d24c3ad7f7a7808bec2a191ba
Arturo-Blanco <103155759+Arturo-Blanco@users.noreply.github.com>
2022-05-07 14:43:20 -0300

    Se modifica "if" anidados por "switch"

--- determinarMedalla/index.ts
+++ determinarMedalla/index.ts
@@ -3,17 +3,18 @@ let btnCalculo = document.getElementById("btnCalculo");
 
 btnCalculo.addEventListener("click", () => {
   let posicionLlegada: number = Number(posicion.value);
-  if (posicionLlegada === 1) {
-    console.log("Entregar medalla de oro");
-  } else {
-    if (posicionLlegada === 2) {
+
+  switch (posicionLlegada) {
+    case 1:
+      console.log("Entregar medalla de oro");
+      break;
+    case 2:
       console.log("Entregar medalla de plata");
-    } else {
-      if (posicionLlegada === 3) {
-        console.log("Entregar medalla de bronce");
-      } else {
-        console.log("Entregar medalla de participacion");
-      }
-    }
+      break;
+    case 3:
+      console.log("Entregar medalla de bronce");
+      break;
+    default:
+      console.log("Entregar mencion de participacion");
   }
 });
